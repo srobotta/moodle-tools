0f2fb11ee2f5d813f72ee0d47dff401261ab4255
Stephan Robotta <stephan.robotta@bfh.ch>
2022-11-01 16:36:48 +0100

    make update script more generic so that it can be used out of the box

--- update_branches.sh
+++ update_branches.sh
@@ -1,34 +1,55 @@
 #!/bin/bash
 
 # For the different MDLs that I am working on, I have a branch in my
-# repo. Whenever upstream (the original Moodle repo where I did the fork)
-# is updated, I need to update my local branches.
+# repo. Whenever upstream (the original Moodle repo where I did the fork
+# from) is updated, I need to update my local branches.
+# git remote -v returns your repo locations. In my case I see:
+# upstream	git://git.moodle.org/moodle.git (fetch)
+# upstream	git://git.moodle.org/moodle.git (push)
+#
 # My branch names follow this convention:
 # - a branch from the current master is trailed by master
 # - a branch from the 4.0 stable branch is trailed by 400
 # - a branch from the 3.11 stable branch is trailed by 311
 # My branches follow this schema: MDL-XXXXX-[master|400|311]
+# The variable MOODLE_DIR can be defined as a variable or
+# passed as an argument and should contain the location to
+# your checked out Moodle git repo.
 
-MOODLE_DIR=~/workspace/moodle
+if [ ! -z $1 ]; then
+  repodir=$1
+elif [ ! -z $MOODLE_DIR ]; then
+  repodir=$MOODLE_DIR
+else
+  repodir=~/workspace/moodle
+fi
+
+if [ ! -d $repodir ]; then
+  echo "Could not determine location of Moodle code"
+  exit 1
+fi
 
 CWD=$(pwd)
-cd $MOODLE_DIR
+cd $repodir
 
 git fetch upstream
  
 branches=$(git branch | tr -d \*)
 for b in $branches; do
   suffix="${b##*-}"
-  git checkout $b
+  upbranch=''
   if [ "$suffix" == "master" ]; then
-    git rebase upstream/master
-    git push origin $b -f
+    upbranch=master
   elif [ "$suffix" == "400" ]; then
-    git rebase upstream/MOODLE_400_STABLE
-    git push origin $b -f
+    upbranch=MOODLE_400_STABLE
   elif [ "$suffix" == "311" ]; then
-    git rebase upstream/MOODLE_311_STABLE
-    git push origin $b -f
+    upbranch=MOODLE_311_STABLE
+  fi
+  
+  if [ "$upbranch " != ' ' ]; then
+    git checkout $b
+    git rebase upstream/$upbranch
+    git push origin $b -f    
   fi
 done
 
